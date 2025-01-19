94e67e08e140e5c93d12ef9af8366328c438a9ec
AaronVasGonz <arjoz988@gmail.com>
2025-01-19 17:54:27 -0600

    changes to build

--- NTW-TEST-PATTERNS.Arquitecture/EnvConfg.cs
+++ NTW-TEST-PATTERNS.Arquitecture/EnvConfg.cs
@@ -1,19 +1,16 @@
-﻿using DotNetEnv;
-using System;
-using System.IO;
+﻿using System;
 
 public class EnvConfig
 {
-    private static readonly string EnvFilePath = @"C:\Users\arjoz\Source\Repos\NTW-TEST-PATTERNS\NTW-TEST-PATTERNS.Arquitecture\.env";
     public static void Initialize()
     {
-        Console.WriteLine($"Cargando el archivo .env desde: {EnvFilePath}");
-        Env.Load(EnvFilePath);
+        // Lee la variable de entorno directamente
+        var smtpServer = Environment.GetEnvironmentVariable("SMTP_SERVER");
 
-        string smtpServer = Env.GetString("SMTP_SERVER");
+        // Verifica si está vacía o nula
         if (string.IsNullOrEmpty(smtpServer))
         {
-            throw new Exception("SMTP_SERVER no está configurado en el archivo .env");
+            throw new Exception("SMTP_SERVER no está configurado en las variables de entorno.");
         }
         else
         {
@@ -21,13 +18,16 @@ public class EnvConfig
         }
     }
 
+    // Método auxiliar para obtener cualquier variable de entorno
     public static string GetEnvVariable(string variable)
     {
-        string envVariable = Env.GetString(variable);
+        string envVariable = Environment.GetEnvironmentVariable(variable);
+
         if (string.IsNullOrEmpty(envVariable))
         {
-            throw new Exception(variable + " no está configurado en el archivo .env");
+            throw new Exception(variable + " no está configurado en las variables de entorno.");
         }
+
         return envVariable;
     }
 }
